bee16cb64f5cbef7b19bd6b121a78dda1f21fc3c
Crassus Crudelis <crudelus@crudelis-P53E.(none)>
2013-06-12 14:02:36 +0200

    finishing the DB connector

--- app/Global.java
+++ app/Global.java
@@ -8,7 +8,7 @@ import models.*;
 
 /**
  * Created with IntelliJ IDEA.
- * User: Crassus
+ * Benutzer: Crassus
  * Date: 09.06.13
  * Time: 17:42
  * To change this template use File | Settings | File Templates.
@@ -17,8 +17,8 @@ public class Global extends GlobalSettings {
 	
     @Override
     public void onStart(Application app) {
-        if(User.find.findRowCount()==0){
-            new User("daniel", "1234", "Daniel Rehmann").save();
+        if(Benutzer.find.findRowCount()==0){
+            new Benutzer("daniel", "1234", "Daniel Rehmann").save();
         }
         if (Archetype.find.findRowCount() == 0) {
         	checkingArchetypes();
--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -12,16 +12,16 @@ create table archetype (
   constraint pk_archetype primary key (id))
 ;
 
-create table user (
+create table benutzer (
   accountname               varchar(255) not null,
   password                  varchar(255),
   full_name                 varchar(255),
-  constraint pk_user primary key (accountname))
+  constraint pk_benutzer primary key (accountname))
 ;
 
 create sequence archetype_seq;
 
-create sequence user_seq;
+create sequence benutzer_seq;
 
 
 
@@ -30,9 +30,9 @@ create sequence user_seq;
 
 drop table if exists archetype cascade;
 
-drop table if exists user cascade;
+drop table if exists benutzer cascade;
 
 drop sequence if exists archetype_seq;
 
-drop sequence if exists user_seq;
+drop sequence if exists benutzer_seq;
 
